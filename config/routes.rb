e261a4a8113dd5db7e3851209be738316ebebd17
Mary Banshee <mary.banshee@gmail.com>
2019-07-24 10:31:36 +0300

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:create, :index, :destroy]
-      resources :projects, :shallow => true do
-        resources :tasks do
-          resources :comments
-        end
-      end
+      resources :projects
       post 'user_token' => 'user_token#create'
     end
   end
