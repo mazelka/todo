e2ea24d37745653398c84cd8668607349771b928
Mary Banshee <mary.banshee@gmail.com>
2019-07-26 15:11:09 +0300

    add prod_url_option in environments

--- config/initializers/url_options.rb
+++ config/initializers/url_options.rb
@@ -1,4 +1,4 @@
-if Rails.env == 'production'
+if Rails.application.config.prod_url_option
   Rails.application.routes.default_url_options[:host] = 'mazelka-todo.herokuapp.com'
 else
   Rails.application.routes.default_url_options[:host] = 'localhost:3000'
